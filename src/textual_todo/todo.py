342c8e7d21f214082959602267c125ec4ba9265a
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-03-15 19:02:10 +0000

    Add action to add items.

--- src/textual_todo/todo.py
+++ src/textual_todo/todo.py
@@ -4,8 +4,16 @@ from textual.app import App, ComposeResult
 from textual.containers import Vertical
 from textual.widgets import Footer
 
+from .todoitem import TodoItem
+
 
 class TODOApp(App[None]):
+    """A simple and elegant TODO app built with Textual."""
+
+    BINDINGS = [
+        ("n", "new_todo", "New"),
+    ]
+
     _todo_container: Vertical
     """Container for all the TODO items that are due."""
 
@@ -13,6 +21,13 @@ class TODOApp(App[None]):
         self._todo_container = Vertical(id="todo-container")
         yield self._todo_container
         yield Footer()
+
+    async def action_new_todo(self) -> None:
+        """Add a new TODO item to the list."""
+        new_todo = TodoItem()
+        await self._todo_container.mount(new_todo)
+        new_todo.scroll_visible()
+        new_todo.set_status_message("Add description and due date.")
 
 
 app = TODOApp()
