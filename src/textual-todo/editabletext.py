f0e7f3ac73c0e1a1a921403098221f5a99b46687
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-03-15 14:45:22 +0000

    Wire EditableText components.

--- src/textual-todo/editabletext.py
+++ src/textual-todo/editabletext.py
@@ -40,13 +40,68 @@ class EditableText(Static):
     }
     """
 
+    _confirm_button: Button
+    """The button to confirm changes to the text."""
+    _edit_button: Button
+    """The button to start editing the text."""
+    _input: Input
+    """The field that allows editing text."""
+    _label: Label
+    """The label that displays the text."""
+
     def compose(self) -> ComposeResult:
-        yield Input(
+        self._input = Input(
             placeholder="Type something...", classes="editabletext--input ethidden"
         )
-        yield Label("", classes="editabletext--label")
-        yield Button("📝", classes="editabletext--edit")
-        yield Button("✅", classes="editabletext--confirm ethidden")
+        self._label = Label("", classes="editabletext--label")
+        self._edit_button = Button("📝", classes="editabletext--edit")
+        self._confirm_button = Button(
+            "✅", classes="editabletext--confirm ethidden", disabled=True
+        )
+
+        yield self._input
+        yield self._label
+        yield self._edit_button
+        yield self._confirm_button
+
+    @property
+    def is_editing(self) -> bool:  # (1)!
+        """Is the text being edited?"""
+        return not self._input.has_class("ethidden")
+
+    def on_button_pressed(self) -> None:  # (2)!
+        if self.is_editing:
+            self.switch_to_display_mode()
+        else:
+            self.switch_to_editing_mode()
+
+    def switch_to_editing_mode(self) -> None:
+        if self.is_editing:
+            return
+
+        self._input.value = str(self._label.renderable)
+
+        self._label.add_class("ethidden")
+        self._input.remove_class("ethidden")
+
+        self._edit_button.disabled = True
+        self._edit_button.add_class("ethidden")
+        self._confirm_button.disabled = False
+        self._confirm_button.remove_class("ethidden")
+
+    def switch_to_display_mode(self) -> None:
+        if not self.is_editing:
+            return
+
+        self._label.renderable = self._input.value
+
+        self._input.add_class("ethidden")
+        self._label.remove_class("ethidden")
+
+        self._confirm_button.disabled = True
+        self._confirm_button.add_class("ethidden")
+        self._edit_button.disabled = False
+        self._edit_button.remove_class("ethidden")
 
 
 class EditableTextApp(App[None]):
