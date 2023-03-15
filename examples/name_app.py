00aaeb56bc4414182c6d9f01799ed05949438c81
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-03-15 16:36:07 +0000

    Log custom events in NameApp.

--- examples/name_app.py
+++ examples/name_app.py
@@ -1,24 +1,37 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Label
+from textual.widgets import Label, TextLog
 
 from textual_todo.editabletext import EditableText
 
 
-class EditableText04App(App[None]):
+class NameApp(App[None]):
+    CSS = """
+    TextLog {
+        height: 1fr;
+    }
+    """
+
     def compose(self) -> ComposeResult:
         yield Label("First name")
-        yield EditableText()
+        yield EditableText(id="first")
         yield Label("Last name")
-        yield EditableText()
+        yield EditableText(id="last")
         yield Label("Preferred name")
-        yield EditableText()
+        yield EditableText(id="preferred")
+        yield TextLog()
 
     def on_mount(self) -> None:
         for editable_text in self.query(EditableText):
             editable_text.switch_to_editing_mode()
 
+    def on_editable_text_edit(self, event: EditableText.Edit) -> None:
+        self.query_one(TextLog).write(f"Editing @ {event.editable_text.id}")
 
-app = EditableText04App()
+    def on_editable_text_display(self, event: EditableText.Display) -> None:
+        self.query_one(TextLog).write(f"Displaying @ {event.editable_text.id}")
+
+
+app = NameApp()
 
 
 if __name__ == "__main__":
